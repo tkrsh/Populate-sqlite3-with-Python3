a965653e0d8424fc6536cb0299c59423adb90d75
tkrsh <utkarsh.srivastava123@outlook.com>
2021-01-08 12:48:26 +0530

    fixed error handling

--- populate.py
+++ populate.py
@@ -1,69 +1,63 @@
-import pandas as pd
+"""
+sqlite3 for read/write operations on the db
+"""
 import sqlite3
+import pandas as pd
 
-def get_cursor(file,table):
+def get_cursor(file, table):
     """
     Returns cursor -> takes file,table prints satus.
     """
     try:
-        
-        connection = sqlite3.connect(file)
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM {}".format(table))
-        print("Successfully Connected to Databse",file)
-        return cursor,connection
-        
-    except:
-        
+        con = sqlite3.connect(file)
+        cur = con.cursor()
+        cur.execute("SELECT * FROM {}".format(table))
+        print("Successfully Connected to Databse", file)
+        return cursor, con
+    except SystemError:
         print("Connection Failed")
-        
-    return cursor,connection
+    return cur, con
 
-def print_status(cursor):
+
+def print_status(cur):
     """
-    Prints status of the cursor 
+    Prints status of the cursor
     """
-    names = [description[0] for description in cursor.description]
-    fetch = cursor.fetchall()
-    lastrow_id= cursor.lastrowid
-    print("Fields-> {}\nData-> {}\nlastrow-> {}".format(names,fetch,lastrow_id))
-    
+    names = [description[0] for description in cur.description]
+    fetch = cur.fetchall()
+    lastrow_id = cur.lastrowid
+    print("Fields-> {}\nData-> {}\nlastrow-> {}".format(names, fetch, lastrow_id))
+
+
 def gen_data(dataframe):
     """
     Takes dataframe object and returns formated data ready for insertion
     """
-    dataframe.fillna(value="not_found",inplace=True)
+    dataframe.fillna(value="not_found", inplace=True)
     return dataframe.values
 
-def write_data(cursor,data,connection):
+
+def write_data(cur, inp_data, inc_connection):
     """
     Perform insertion takes cursor and and prints status.
-    
     """
-    action = '''insert into {} values ({})'''.format(table,("?,"*9)[:-1])
-    
-    for i in data:
-        cursor.execute(action, i)
-    
-    connection.commit()
+    action = '''insert into {} values ({})'''.format(TABLE, ("?," * 9)[:-1])
+    for i in inp_data:
+        cur.execute(action, i)
+    inc_connection.commit()
+
 
 if __name__ == "__main__":
-    
-     
-    df=pd.read_csv("codeforces.csv")    
-    df=df[['Unnamed: 0','username','rating_codeforces','name','city','country','organization','rank_codeforces','leaderboard_rank']]
-    file='/home/tkrsh/Desktop/db.sqlite3'
-    table='main_ratings'
-
+    df = pd.read_csv("codeforces.csv")
+    df = df[['Unnamed: 0', 'username', 'rating_codeforces', 'name', 'city',
+             'country', 'organization', 'rank_codeforces', 'leaderboard_rank']]
+    FILE = '/home/tkrsh/Desktop/db.sqlite3'
+    TABLE = 'main_ratings'
     try:
-        
-        cursor,connection=get_cursor(file,table) 
+        cursor, connection = get_cursor(FILE, TABLE)
         print_status(cursor)
-        data=gen_data(df)
-        write_data(cursor,data,connection)
+        data = gen_data(df)
+        write_data(cursor, data, connection)
         print("Successfull Insertions")
-    
-    except:
+    except OverflowError:
         print("Error Occured")
-    
-    
